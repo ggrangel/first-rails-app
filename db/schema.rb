09bf717065abb9856dd6394f55d16e3ee365b240
Gustavo Rangel <gustavorangel91@gmail.com>
2022-04-19 10:56:01 -0300

    Create a relationship where a user can have many posts.

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_19_134836) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
